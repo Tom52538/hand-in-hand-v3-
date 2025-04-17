2a3e4a63bc880edd30fe1e16b5c0132e94f87c28
Tom52538 <tg1967mail@gmail.com>
2025-04-17 16:25:41 +0200

    Update monthlyPdfEndpoint.js

--- routes/monthlyPdfEndpoint.js
+++ routes/monthlyPdfEndpoint.js
@@ -84,7 +84,7 @@ function drawDocumentHeader(doc, title, name, startDate, endDate) {
 
     try {
         const logoPath = path.join(process.cwd(), 'public', 'icons', 'Hand-in-Hand-Logo-192x192.png');
-        doc.image(logoPath, doc.page.width - right - 77, y, { width: 77, height: 77 });
+        doc.image(logoPath, doc.page.width - right - 90, y, { width: 90, height: 90 });
     } catch (e) { console.warn("Logo konnte nicht geladen/gezeichnet werden:", e.message); }
 
     doc.font(FONT_BOLD).fontSize(FONT_SIZE.HEADER).fillColor('black');
